155f64df5338418a69ba370b17c38c377b4a48b1
khaja shija uddin <83953366+mudasser131@users.noreply.github.com>
2023-12-01 23:38:37 +0530

    Add files via upload

---
+++ OTP genrator/pro.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn");
+
+ otp = () => {
+ var digits = "0123456789"
+var OTP ="";
+
+for( var i=0;i<4;i++){
+
+OTP = OTP+digits[Math.floor(Math.random()*10)]
+
+}
+
+document.getElementById("otp").innerHTML= " Your OTP is  " +OTP;
+
+
+}
+
